72c893132dd9e172303425c5b72d6564a372a7e5
Shalom Dawit <Shalom.Dawit@gmail.com>
2026-05-07 23:46:02 -0400

    nested Ashi component into Documents component

---
+++ client/src/components/Ashi/index.js
@@ -0,0 +1,44 @@
+
+function Documents() {
+
+     return (
+    <>
+
+<center><h2><u>Techniques of the Russo Ashi Gatame No Kata</u></h2></center>
+
+<h4>This kata is performed in sets of three techniques. Special care must be taken when applying such technankles to the ankle and knee. 
+
+NOTE:  The techniques below are not listed in any particular order</h4>
+
+<div>
+<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/ankle1.jpg")}/>Technique1</li>
+
+<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/ankle2.jpg")}/>Technique2</li>
+
+<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/ankle3.jpg")}/>Technique3</li>
+
+<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/ankle4.jpg")}/>Technique4</li>
+
+<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/ankle5.jpg")}/>Technique5</li>
+<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/ankle5.jpg")}/>Technique6 (to come)</li>
+
+<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/ankle5.jpg")}/>Technique7 (to come)</li>
+
+<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee1.jpg")}/>Technique8</li>
+<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee2.jpg")}/>Technique9</li>
+<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee3.jpg")}/>Technique10</li>
+<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee4.jpg")}/>Technique11</li>
+<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee5.jpg")}/>Technique12</li>
+<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee6.jpg")}/>Technique13</li>
+<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee7.jpg")}/>Technique14</li>
+<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee8.jpg")}/>Technique15</li>
+<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee9.jpg")}/>Technique16</li>
+<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee9.jpg")}/>Technique17 (to come)</li>
+<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee9.jpg")}/>Technique18 (to come)</li>
+</div>
+
+    </>
+  );
+}
+
+export default Documents;
