ece25415cca8bf67db9eaec61931878fa50bbe4f
Shalom Dawit <Shalom.Dawit@gmail.com>
2026-05-13 14:52:22 -0400

    updated styling inside index.js for AttackCombos component

--- client/src/components/AttackCombos/index.js
+++ client/src/components/AttackCombos/index.js
@@ -6,24 +6,24 @@ function Documents() {
 
     return (
         <>
-
-            <p ALIGN="center">
+<h2>
+            <p align="center">
                 <u><b>IKKYO NO KATA A: Attack (Kogeki) Combinations (Renraku)  For Distance Fighting</b></u>
-            </p>
+            </p></h2>
 
-            <center><h2>Andrew Yiannakis, ph.D.<br />
+            <center>Andrew Yiannakis, ph.D.<br />
                 Founder,  Wa Shin Ryu Jujutsu<br />
                 7th Dan, Jujutsu<br />
-                6th Dan Judo</h2></center>
-            <center><h2>Greg Kane, ph.D.<br />4th Dan Wa Shin Ryu Jujutsu</h2></center>
+                6th Dan Judo</center>
+            <center>Greg Kane, ph.D.<br />4th Dan Wa Shin Ryu Jujutsu</center>
 
 
             <p>NOTE: Techniques are to be practised on both sides</p>
-            <h4>v1.19/  12/06/2009</h4>
+            <p>v1.19  (12/06/2009)</p>
 
 
-            <h2><u>Introduction</u></h2>
-            <p><h2><p>In distance fighting it is sometimes necessary to initiate an attack in order to terminate quickly a life threatening or
+           <center> <h2><u>Introduction</u></h2></center>
+            <p>In distance fighting it is sometimes necessary to initiate an attack in order to terminate quickly a life threatening or
                 combative situation that is already in progress. In Wa Shin Ryu Jujutsu such strategic forms of attack may involve a single (Level I)
                 technique (e.g., a direct kick to the groin), a combination involving the use of two (Level II) techniques (e.g., a fake kick to the groin and
                 then a backhand strike to the face), or, as in Level III, by, for example, a downward strike to the head followed by a knee strike
@@ -39,65 +39,65 @@ function Documents() {
                     combinations for distance fighting.  Students are therefore encouraged to <u>master these</u> and then experiment, go beyond, and
                     discover new ways of engaging the assailant by developing their own sets of combinations.</p>
 
-                As you may expect, close quarter and ground fighting also rely heavily on the use of combinations to
+               <p> As you may expect, close quarter and ground fighting also rely heavily on the use of combinations to
                 outwit the opponent. In many respects, advanced training is the struggle to master the science and
                 art of combat by mastering the application of combinations. The skills involved in this type of
                 training must be practiced in the context of the principles of Heiho (principles of Combat Strategy),
-                Distance, Close Quarter and Ground Fighting.</h2></p>
-
+                Distance, Close Quarter and Ground Fighting.</p>
+     {/* ------------------------------------------------------------------- */}
             <center><h2><u>Level II Attack Combinations (involving the application of two techniques)</u></h2></center>
 
 
 
-            <h2><b><u><p ALIGN="JUSTIFY"><li><u>1. Hidari Shomen Tsuki to Migi Seiken Uchi to ribcage.</u>: From
+           <p align="justify"><li>1.<u> HIDARI SHOMEN TSUKI to MIGI SEIKEN UCHI to ribcage</u>: From
                 left attack posture fake punch to face with right fist (draw it back like the string of a bow). Ensure that
                 Uke starts to track this action with his/her eyes.  Then deliver a short jab to the face with left fist and as
-                Uke brings hands up to his face to defend quickly follow through with a Seiken Uchi to his/her ribcage or solar plexus.</li></p></u></b>
+                Uke brings hands up to his face to defend quickly follow through with a Seiken Uchi to his/her ribcage or solar plexus.</li></p>
 
-                <b><u><p ALIGN="JUSTIFY"><li><u>2. MAE GERI to URAKEN UCHI</u>: From right attack posture fake right leg mae geri to
+               <p align="justify"><li>2.<u> MAE GERI to URAKEN UCHI</u>: From right attack posture fake right leg mae geri to
                     groin then deliver right backfist uraken to face</li></p>
-                    <p ALIGN="JUSTIFY"><li><u>3. URAKEN UCHI to MAE GERI</u>: From right attack posture fake right backfist uraken to
-                        face and deliver right mae geri to groin (Uke is in Migi Gamae)</li></p></u></b>
 
-                <p ALIGN="JUSTIFY"><li><u>4. Left hand TSUKI (jab) to Uke's face to SEIKEN UCHI</u>. In
+                    <p align="justify"><li>3.<u> URAKEN UCHI to MAE GERI</u>: From right attack posture fake right backfist uraken to
+                        face and deliver right mae geri to groin (Uke is in Migi Gamae)</li></p>
+
+                <p align="justify"><li>4.<u> Left hand TSUKI (jab) to UKE's face to SEIKEN UCHI</u>: In
                     left fighting stance deliver fake jab (TSUKI) to face with left fist. As they raise their arms attack with
                     right seiken uchi to solar plexus (use center for more power)</li></p>
 
-                <p ALIGN="JUSTIFY"><li><u>5. TETSUI UCHI to MAWASHI GERI</u> (or HIZA GERI): In right attack posture step
+                <p align="justify"><li>5.<u> TETSUI UCHI to MAWASHI GERI</u> (or HIZA GERI): In right attack posture step
                     forward leading with left foot and deliver downward hammer fist to head with left  hammer fist.  As
                     opponent receives blow with upraised arms deliver leg strike with right knee to his/her ribcage</li></p>
 
-                <p ALIGN="JUSTIFY"><li><u>6. JODAN URAKEN UCHI to Gedan URAKEN</u>: From right attack posture fake right backfist uraken to
+                <p align="justify"><li>6.<u> JODAN URAKEN UCHI to Gedan URAKEN</u>: From right attack posture fake right backfist uraken to
                     face and deliver right backfist uraken to groin.  For protection, Tori raises own left arm above and
                     slightly in front of own face.  The final phase shows a nice circular movement upwards to protect own face</li></p>
 
-                <p ALIGN="JUSTIFY"><li><u>7. Left hand TSUKI to face to CHUDAN MAWASHI GERI to
+                <p align="justify"><li>7.<u> Left hand TSUKI to face to CHUDAN MAWASHI GERI to
                     rib cage</u>: Tori attacks Uke's face with left hand jab (Tsuki) and as
                     Uke defends by bringing arms up to the face Tori steps in for a
                     right side mawashi geri (with shin or instep) to Uke's ribcage. Tori jabs by
                     stepping forward with left foot.
                 </li></p>
 
-                <p ALIGN="JUSTIFY"><li><u>8. Downward strike (Migi OROSHI UCHI) with own open hand  to
-                    Migi MAE GERI</u>.  In lright attack posture (migi gamae)  Tori makes a show of raising own
+                <p align="justify"><li>8.<u> Downward strike (MIGI OROSHI UCHI) with own open hand  to
+                    Migi MAE GERI</u>:  In lright attack posture (migi gamae)  Tori makes a show of raising own
                     right arm as though intending to strike downwards to the top of Uke's head with open hand,
                     palm facing down. Tori ensures that Uke tracks movement with his/her eyes.
                     Tori then delivers mae geri to groin with right instep or ball of foot</li></p>
 
-                <p ALIGN="JUSTIFY"><li><u>9. CHUDAN YOKO MAWASHI GERI into
-                    left GEDAN MAWASHI GERI</u>: In right attack posture step in a spinning fashion in
+                <p align="justify"><li>9.<u> CHUDAN YOKO MAWASHI GERI into left GEDAN MAWASHI GERI</u>: In right attack posture step in a spinning fashion in
                     front of opponent leading with left foot and swivel right leg around bent at the knee.
                     place right foot on floor and deliver left side GEDAN MAWASHI GERI to the outside of
                     Uke's right knee (or just above).  Important to deceive Uke into thinking that you are
                     moving in to attack the left side of their body.</li></p>
 
-                <p ALIGN="JUSTIFY"><li><u>10. MAWASHI GERI to URAKEN UCHI</u>: From
+                <p align="justify"><li>10.<u> MAWASHI GERI to URAKEN UCHI</u>: From
                     right attack posture (Migi Gamae)  Tori steps in with left foot (turning to left about 45 degrees) and
-                    delivers a fake mawashi geri to outside of opponent�s left leg, just above Uke's knee, with own
+                    delivers a fake mawashi geri to outside of opponent's left leg, just above Uke's knee, with own
                     right instep.  Uke picks own leg up to evade kick. Tori continues turning in
-                    anti-clockwise (take big step)  and delivers uraken to uke�s temple/ear with left backfis (Uraken Uchi)</li></p>
+                    anti-clockwise (take big step)  and delivers uraken to uke's temple/ear with left backfis (Uraken Uchi)</li></p>
 
-                <p ALIGN="JUSTIFY"><li><u>11. GEDAN MAWASHI GERI to TANIOTOSHI</u>.  Start in
+                <p align="justify"><li>11.<u> GEDAN MAWASHI GERI to TANIOTOSHI</u>:  Start in
                     right attack posture and step toward Uke with left foot. Turn left foot about
                     30 degrees to the left and position it about a foot and a half directly in
                     front of Uke's left foot. Deliver Gedan Mawashi Geri with right shin or
@@ -106,26 +106,26 @@ function Documents() {
                     the ground and ends up with own left shoulder over Uke's left shoulder. Both Tori's arms are
                     used to secure and take Uke down.</li></p>
 
-                <p ALIGN="JUSTIFY"><li><u>12. YOKO MEN UCHI to USHIRO EMpI UCHI</u>: Tori attacks Uke's left temple with
+                <p align="justify"><li>12.<u> YOKO MEN UCHI to USHIRO EMPI UCHI</u>: Tori attacks Uke's left temple with
                     right hand tegatana (shuto) (yoko men uchi), stepping in with right foot.  As Uke draws arms up for
                     protection Tori lowers center of gravity and continues rotation anticlockwise in front of Uke and
                     ends up facing away from Uke (feet facing away from Uke).  Tori strikes Uke's left rib cage with
                     hidari ushiro empi uchi.  Important to continue rotation into the elbow strike</li></p>
 
 
-                <p ALIGN="JUSTIFY"><li><u>13. GEDAN MAWASHI GERI to USHIRO URAKEN UCHI</u>. Tori starts in
+                <p align="justify"><li>13.<u> GEDAN MAWASHI GERI to USHIRO URAKEN UCHI</u>: Tori starts in
                     left attack posture (hidari gamae).  Uke is in migi gamae. Tori delivers gedan mawashi geri to
                     Uke's right inner thigh just above knee.  As Uke begins to collapse to his/her right Tori delivers a
                     right handed elbow strike (ushiro empi uchi) to Uke's left side of the head. Tori then steps in with
                     left foot and takes Uke down with a hidari kosoto gake..</li></p>
 
-                <p ALIGN="JUSTIFY"><li><u>14. YOKO MEN UCHI to TANI OTOSHI</u>i. Tori starts from
+                <p align="justify"><li>14.<u> YOKO MEN UCHI to TANI OTOSHI</u>: Tori starts from
                     left attack posture (hidari gamae) and steps in with right leg and positions own right foot just
                     to the outside of Uke's left.  Attacks Uke's left temple with right hand tegatana (shuto) (yoko men uchi).  As
                     Uke draws arms up for protection Tori drives own right leg deep behind Uke's both feet and
                     executes tani otoshi (valley drop). </li></p>
 
-                <p ALIGN="JUSTIFY"><li><u>15. Sliding YOKO MAWASHI GERI to ASHI GARAME</u>. Uke attacks Tori with
+                <p align="justify"><li>15.<u> Sliding YOKO MAWASHI GERI to ASHI GARAME</u>: Uke attacks Tori with
                     right leg using high mawashi geri to chest or head regions.  Tori who stands in migi gamae slides in
                     low leading with right leg and drops in front of Uke's supporting leg (the left one).
                     Delivers mawashi geri against back of Uke's left knee with own left shin.  As
@@ -134,7 +134,7 @@ function Documents() {
                     right knee up (trapping Uke's left leg between own legs).  Tori then
                     seizes Uke's left little toe and applies joint lock </li></p>
 
-                <p ALIGN="JUSTIFY"> <li><u>16. YOKO MEN UCHI to GEDAN MAWASHI GERI</u>. Tori starts from
+                <p align="justify"> <li>16.<u> YOKO MEN UCHI to GEDAN MAWASHI GERI</u>: Tori starts from
                     left attack posture (hidari gamae) and steps in with right foot and positions own right foot just to
                     the outside of Uke's left.  Attacks Uke's left temple with right hand tegatana (shuto) (yoko men uchi).
                     As Uke draws arms up for protection Tori sinks very low on leading leg (almost in single leg squat position) and
@@ -143,17 +143,17 @@ function Documents() {
                     floor for support. After completing the sweep Tori ends up in migi gamae (for this technique to
                     work effectively Tori must speed up and continue the turn until both of Uke's legs have been swept away).
                     Bulding up lots of momentum is therefore critical in this technique.</li> </p>
-            </h2>
+            {/* --------------------------------------------------------------- */}
 
-            <li><center><h2><u>Level III Attack Combinations</u></h2></center><p>These are a continuation of
+          <center><h2><u>Level III Attack Combinations</u></h2></center><p>These are a continuation of
                 Level II attacks.  They generally follow through into close quarter fighting in the standing position or
-                on the ground.</p>  </li>
+                on the ground.</p>  
 
-            <li><center><h2><u>Level IV Attack Combinations and Counters</u></h2></center><p>These are
-                a continuation of Level III attacks but Uke manages to block, deflect or evade Tori's
+            <center><h2><u>Level IV Attack Combinations and Counters</u></h2></center>
+            <p>These are a continuation of Level III attacks but Uke manages to block, deflect or evade Tori's
                 second strike thus necessitating an adaptation response on Tori's part.  After
                 handling Uke's response Tori then follows through into close quarter fighting in
-                the standing position or on the ground.</p>  </li>
+                the standing position or on the ground.</p>  
 
         </>
     );
