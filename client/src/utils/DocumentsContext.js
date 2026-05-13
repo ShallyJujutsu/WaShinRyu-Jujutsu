d6b3388e758cea858e96559a2dd03fa12d820769
Shalom Dawit <Shalom.Dawit@gmail.com>
2026-05-13 14:27:53 -0400

    moved images and video. updated documentList images and order.

--- client/src/utils/DocumentsContext.js
+++ client/src/utils/DocumentsContext.js
@@ -8,32 +8,41 @@ export const DocumentsProvider = props => {
     const [documents, setDocuments] = useState([
         
 
-        {
-            
-            name: "Ashi No Kata",
-             descriptionShort: "Techniques of the Russo Ashi Gatame No Kata",
-            imageURL: require("../assets/img/contextPics/ankle1.jpg"),
-           deployURL: "Ashi",
-            //skills: "",
-            id:1
-        },
-        {
-            
-             name: "IKKYO NO KATA A: Attack (Kogeki) Combinations (Renraku)",
-             descriptionShort: "Attack Combinations Kata for Distance Fighting",
-             imageURL: require("../assets/img/contextPics/ankle2.jpg"),
-             deployURL:"AttackCombos",
-            //skills: "",
-             id:2
-        },
+       
+       
         {
 
             name:"IKKYO NO KATA B",
             descriptionShort:"Framework to the First Set of Principles - Distance Fighting",
-            imageURL: require("../assets/img/archivedPics/hillock.gif"),
+            imageURL: require("../assets/img/contextPics/shihoonfrye.jpg"),
             deployURL:"IkkyoNoKata",
             //skills:"",
+            id:1
+        },
+           {
+            name:"Nage No Kata (2)",
+            descriptionShort: "Ura or Combat Version: Throws From The Clinch Position",
+            imageURL: require("../assets/img/contextPics/gralyn.gif"),
+            deployURL: "NageNoKata2",
+            //skills:"",
+            id:2
+        },
+        {
+            name:"Sankyo No Kata",
+            descriptionShort: "Ura or Combat Version: Throws From The Clinch Position",
+            imageURL: require("../assets/img/contextPics/pic06.jpg"),
+            deployURL: "SankyoNoKata",
+            //skills:"",
             id:3
+        },
+         {
+            
+             name: "IKKYO NO KATA A: Attack (Kogeki) Combinations (Renraku)",
+             descriptionShort: "Attack Combinations Kata for Distance Fighting",
+             imageURL: require("../assets/img/contextPics/Image3.jpg"),
+             deployURL:"AttackCombos",
+            //skills: "",
+             id:4
         },
         {
             name:"Jigoku No Kata",
@@ -41,30 +50,24 @@ export const DocumentsProvider = props => {
             imageURL: require("../assets/img/contextPics/jigoku.jpg"),
             deployURL: "JigokuNoKata",
             //skills:"",
-            id:4
+            id:5
         },
           {
             name:"Nage No Kata (1)",
             descriptionShort: "Nikyo No Kata A (Omote Version)",
-            imageURL: require("../assets/img/contextPics/Throw1.jpg"),
+            imageURL: require("../assets/img/contextPics/Image7.jpg"),
             deployURL: "NageNoKata1",
-            //skills:"",
-            id:5
-        },
-         {
-            name:"Nage No Kata (2)",
-            descriptionShort: "Ura or Combat Version: Throws From The Clinch Position",
-            imageURL: require("../assets/img/contextPics/Throw2.jpg"),
-            deployURL: "NageNoKata2",
             //skills:"",
             id:6
         },
-  {
-            name:"Sankyo No Kata",
-            descriptionShort: "Ura or Combat Version: Throws From The Clinch Position",
-            imageURL: require("../assets/img/contextPics/pic06.jpg"),
-            deployURL: "SankyoNoKata",
-            //skills:"",
+  
+         {
+            
+            name: "Ashi No Kata",
+             descriptionShort: "Techniques of the Russo Ashi Gatame No Kata",
+            imageURL: require("../assets/img/contextPics/ankle1.jpg"),
+           deployURL: "Ashi",
+            //skills: "",
             id:7
         },
  
