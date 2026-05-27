c0b664ca4624098ae4cc00ad7712e9a8c5ca660f
Shalom Dawit <Shalom.Dawit@gmail.com>
2026-05-26 22:29:29 -0400

    for Ashi component: format & changed required prop to import for ES6

--- client/src/components/Ashi/index.js
+++ client/src/components/Ashi/index.js
@@ -1,42 +1,114 @@
+import ankle1 from "../../assets/img/contextPics/ankle1.jpg";
+import ankle2 from "../../assets/img/contextPics/ankle2.jpg";
+import ankle3 from "../../assets/img/contextPics/ankle3.jpg";
+import ankle4 from "../../assets/img/contextPics/ankle4.jpg";
+import ankle5 from "../../assets/img/contextPics/ankle5.jpg";
+import nopho from "../../assets/img/contextPics/nopho.jpg";
+import knee1 from "../../assets/img/contextPics/knee1.jpg";
+import knee2 from "../../assets/img/contextPics/knee2.jpg";
+import knee3 from "../../assets/img/contextPics/knee3.jpg";
+import knee4 from "../../assets/img/contextPics/knee4.jpg";
+import knee5 from "../../assets/img/contextPics/knee5.jpg";
+import knee6 from "../../assets/img/contextPics/knee6.jpg";
+import knee7 from "../../assets/img/contextPics/knee7.jpg";
+import knee8 from "../../assets/img/contextPics/knee8.jpg";
+import knee9 from "../../assets/img/contextPics/knee9.jpg";
 
 function Documents() {
-
-     return (
+  return (
     <>
+      <center>
+        <h2>
+          <u>Techniques of the Russo Ashi Gatame No Kata</u>
+        </h2>
+      </center>
 
-<center><h2><u>Techniques of the Russo Ashi Gatame No Kata</u></h2></center>
+      <h4>
+        This kata is performed in sets of three techniques. Special care must be
+        taken when applying such technankles to the ankle and knee. NOTE: The
+        techniques below are not listed in any particular order
+      </h4>
 
-<h4>This kata is performed in sets of three techniques. Special care must be taken when applying such technankles to the ankle and knee. 
+      <div>
+        <li>
+          <img alt={"The Technique"} src={ankle1} />
+          Technique1
+        </li>
 
-NOTE:  The techniques below are not listed in any particular order</h4>
+        <li>
+          <img alt={"The Technique"} src={ankle2} />
+          Technique2
+        </li>
 
-<div>
-<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/ankle1.jpg")}/>Technique1</li>
+        <li>
+          <img alt={"The Technique"} src={ankle3} />
+          Technique3
+        </li>
 
-<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/ankle2.jpg")}/>Technique2</li>
+        <li>
+          <img alt={"The Technique"} src={ankle4} />
+          Technique4
+        </li>
 
-<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/ankle3.jpg")}/>Technique3</li>
+        <li>
+          <img alt={"The Technique"} src={ankle5} />
+          Technique5
+        </li>
+        <li>
+          <img alt={"The Technique"} src={nopho} />
+          Technique6 (to come)
+        </li>
 
-<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/ankle4.jpg")}/>Technique4</li>
+        <li>
+          <img alt={"The Technique"} src={nopho} />
+          Technique7 (to come)
+        </li>
 
-<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/ankle5.jpg")}/>Technique5</li>
-<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/nopho.jpg")}/>Technique6 (to come)</li>
-
-<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/nopho.jpg")}/>Technique7 (to come)</li>
-
-<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee1.jpg")}/>Technique8</li>
-<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee2.jpg")}/>Technique9</li>
-<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee3.jpg")}/>Technique10</li>
-<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee4.jpg")}/>Technique11</li>
-<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee5.jpg")}/>Technique12</li>
-<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee6.jpg")}/>Technique13</li>
-<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee7.jpg")}/>Technique14</li>
-<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee8.jpg")}/>Technique15</li>
-<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/knee9.jpg")}/>Technique16</li>
-<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/nopho.jpg")}/>Technique17 (to come)</li>
-<li><img alt={"The Technique"} src={require("../../assets/img/contextPics/nopho.jpg")}/>Technique18 (to come)</li>
-</div>
-
+        <li>
+          <img alt={"The Technique"} src={knee1} />
+          Technique8
+        </li>
+        <li>
+          <img alt={"The Technique"} src={knee2} />
+          Technique9
+        </li>
+        <li>
+          <img alt={"The Technique"} src={knee3} />
+          Technique10
+        </li>
+        <li>
+          <img alt={"The Technique"} src={knee4} />
+          Technique11
+        </li>
+        <li>
+          <img alt={"The Technique"} src={knee5} />
+          Technique12
+        </li>
+        <li>
+          <img alt={"The Technique"} src={knee6} />
+          Technique13
+        </li>
+        <li>
+          <img alt={"The Technique"} src={knee7} />
+          Technique14
+        </li>
+        <li>
+          <img alt={"The Technique"} src={knee8} />
+          Technique15
+        </li>
+        <li>
+          <img alt={"The Technique"} src={knee9} />
+          Technique16
+        </li>
+        <li>
+          <img alt={"The Technique"} src={nopho} />
+          Technique17 (to come)
+        </li>
+        <li>
+          <img alt={"The Technique"} src={nopho} />
+          Technique18 (to come)
+        </li>
+      </div>
     </>
   );
 }
